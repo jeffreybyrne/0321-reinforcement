2fbf7ac274ea6346e538e5e70f056b5e0d4a8843
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-21 10:41:48 -0400

    used another api, printing those results out as well

--- exercise.py
+++ exercise.py
@@ -1,8 +1,18 @@
-import json
+
 import requests
 
 ottawa_wards_response = requests.get('https://represent.opennorth.ca/boundaries/?sets=ottawa-wards')
 ottawa_wards_response_json = ottawa_wards_response.json()
 
+print("Ottawa Wards:")
 for item in ottawa_wards_response_json['objects']:
+    print(item['name'])
+
+print("======")
+
+rep_set_response = requests.get('https://represent.opennorth.ca/representative-sets/')
+rep_set_response_json = rep_set_response.json()
+
+print("Representative Sets:")
+for item in rep_set_response_json['objects']:
     print(item['name'])
